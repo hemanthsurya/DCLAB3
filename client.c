5661186845bfb1e5e4de6e67ac565e005e33e4d9
hemanth <khemanthsura@gmail.com>
2025-12-14 23:54:48 +0100

    Client and server connection implementation
    Implementation of network handshake and protocol implementation between the client and the server.

--- client.c
+++ client.c
@@ -1,21 +1,131 @@
 #include <stdio.h>
 #include <stdlib.h>
-/* You will to add includes here */
+#include <errno.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/select.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <termios.h>
+#include <regex.h>
 
+#define MAX_NICK 12
+#define READBUF 4096
+
+static struct termios orig_term;
+static int sockfd = -1;
+static char mynick[MAX_NICK + 1];
+
+static void restore_terminal(void) {
+  tcsetattr(STDIN_FILENO, TCSANOW, &orig_term);
+}
+
+static void die(const char *msg) {
+  perror(msg);
+  fflush(stderr);
+  restore_terminal();
+  if (sockfd >= 0) close(sockfd);
+  exit(EXIT_FAILURE);
+}
+
+static int validate_nick(const char *nick) {
+  regex_t re;
+  if (!nick || strlen(nick) == 0 || strlen(nick) > MAX_NICK)
+    return 0;
+  regcomp(&re, "^[A-Za-z0-9_]{1,12}$", REG_EXTENDED | REG_NOSUB);
+  int ok = (regexec(&re, nick, 0, NULL, 0) == 0);
+  regfree(&re);
+  return ok;
+}
+
+static int connect_to_server(const char *host, const char *port) {
+  struct addrinfo hints = {0}, *res, *rp;
+  hints.ai_family   = AF_UNSPEC;
+  hints.ai_socktype = SOCK_STREAM;
+
+  if (getaddrinfo(host, port, &hints, &res) != 0)
+    return -1;
+
+  int s = -1;
+  for (rp = res; rp; rp = rp->ai_next) {
+    s = socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol);
+    if (s == -1) continue;
+    if (connect(s, rp->ai_addr, rp->ai_addrlen) == 0) break;
+    close(s);
+    s = -1;
+  }
+  freeaddrinfo(res);
+  return s;
+}
 
 int main(int argc, char *argv[]){
   
-  /*use select to read from the server and STDIN*/
-
-  if (argc < 3) {
-    printf("Too few arguments!\nExpected: <server-ip>:<server:port> <nickname>\n");
-    return 1;
+ if (argc == 2)
+  {
+    fprintf(stderr, "Too few arguments!\nExpected: <server-ip>:<server-port> <nickname>\n");
+    fflush(stderr);
+    return EXIT_FAILURE;
+  }
+    
+    
+  if (argc != 3) {
+    fprintf(stderr, "Usage: %s host:port nick\n", argv[0]);
+    fflush(stderr);
+    return EXIT_FAILURE;
   }
 
-  if (argc > 3) {
-    printf("Too many arguments!\nExpected: <server-ip>:<server:port> <nickname>\n");
-    return 1;
+  char *hp = strdup(argv[1]);
+  char *colon = strrchr(hp, ':');
+  if (!colon || !validate_nick(argv[2])) {
+    fprintf(stderr, "Invalid arguments\n");
+    fflush(stderr);
+    free(hp);
+    return EXIT_FAILURE;
   }
-  
+
+  *colon = '\0';
+  char *host = hp;
+  char *port = colon + 1;
+  strncpy(mynick, argv[2], sizeof(mynick) - 1);
+
+  sockfd = connect_to_server(host, port);
+  if (sockfd < 0) die("connect");
+
+  /* ---- client prints ---- */
+  printf("Connected to %s:%s\n", host, port);
+  fflush(stdout);
+
+  free(hp);
+
+  /* ---- read HELLO ---- */
+  char buf[READBUF];
+  ssize_t n = read(sockfd, buf, sizeof(buf) - 1);
+  if (n <= 0) die("read");
+  buf[n] = '\0';
+
+  printf("Server protocol: %s", buf);
+  fflush(stdout);
+
+  printf("Protocol supported, sending nickname\n");
+  fflush(stdout);
+
+  char nickmsg[64];
+  snprintf(nickmsg, sizeof(nickmsg), "NICK %s\n", mynick);
+  if (write(sockfd, nickmsg, strlen(nickmsg)) < 0)
+    die("write");
+
+  n = read(sockfd, buf, sizeof(buf) - 1);
+  if (n <= 0) die("read");
+  buf[n] = '\0';
+
+  if (strncmp(buf, "OK", 2) != 0) {
+      fprintf(stderr, "%s", buf);
+      fflush(stderr);
+      return EXIT_FAILURE;
+  }
+
+  printf("Name accepted!\n");
+  fflush(stdout);
 
 }
--- server.c
+++ server.c
@@ -8,74 +8,63 @@
 #include <netdb.h>
 #include <poll.h>
 
+static int listenfd;
 
-int main(int argc, char *argv[]){
-  if (argc != 2)
-  {
-    printf("Expected: <ip>:<port>\n");
-  }
-  
-  char delim[] = ":";
-  char *Desthost = strtok(argv[1], delim);
-  char *Destport = strtok(NULL, delim);
-  int port = atoi(Destport);
-  if (!Desthost || !Destport) {
-    printf("Wrong input arguments\n");
-    return 1;
-  }
-
-  struct addrinfo hints, *res, *rp;
-  memset(&hints, 0, sizeof(hints));
-  hints.ai_family = AF_UNSPEC;
+static int setup_listener(const char *host, const char *port) {
+  struct addrinfo hints = {0}, *res, *rp;
+  hints.ai_family   = AF_UNSPEC;
   hints.ai_socktype = SOCK_STREAM;
-  hints.ai_flags = AI_PASSIVE;
+  hints.ai_flags    = AI_PASSIVE;
 
-  int err = getaddrinfo(Desthost, Destport, &hints, &res);
-  if (err != 0) {
-#ifdef DEBUG
-    printf("getaddrinfo\n");
-#endif
-    return 1;
-  }
-  int listen_fd = -1;
+  if (getaddrinfo(host, port, &hints, &res) != 0)
+    return -1;
+
+  int s = -1;
+  int opt = 1;
+
   for (rp = res; rp; rp = rp->ai_next) {
-    listen_fd = socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol);
-    if (listen_fd == -1)
-      continue;
-    int i = 1;
-    setsockopt(listen_fd, SOL_SOCKET, SO_REUSEADDR, &i, sizeof(i));
-    if (bind(listen_fd, rp->ai_addr, rp->ai_addrlen) == 0)
+    s = socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol);
+    if (s == -1) continue;
+
+    setsockopt(s, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+
+    if (bind(s, rp->ai_addr, rp->ai_addrlen) == 0)
       break;
-    close(listen_fd);
-    listen_fd = -1;
+
+    close(s);
+    s = -1;
   }
+
   freeaddrinfo(res);
 
-  if (listen_fd == -1) {
-#ifdef DEBUG
-    printf("ERROR: BIND FAILED");
-#endif
-    return 1;
+  if (s == -1) return -1;
+  if (listen(s, 16) == -1) {
+    close(s);
+    return -1;
+  }
+  return s;
+}
+
+int main(int argc, char *argv[]){
+if (argc != 2) {
+  fprintf(stderr, "Usage: %s host:port\n", argv[0]);
+  fflush(stderr);
+  return EXIT_FAILURE;
   }
 
-  if (listen(listen_fd, 5) < 0) {
-#ifdef DEBUG
-    printf("ERROR: LISTEN FAILED");
-#endif
-    close(listen_fd);
-    return 1;
+  char *hp = strdup(argv[1]);
+  if (!hp) return EXIT_FAILURE;
+
+  char *colon = strrchr(hp, ':');
+  if (!colon) {
+    free(hp);
+    return EXIT_FAILURE;
   }
 
-  printf("Server listening on %s:%d\n", Desthost, port);
- 
-  while (1) {
-    struct sockaddr_storage client_addr;
-    socklen_t client_len = sizeof(client_addr);
-    int connection_fd =
-        accept(listen_fd, (struct sockaddr *)&client_addr, &client_len);
-    if (connection_fd < 0) {
-      printf("Connection fd error\n");
-      continue;
-    }
-  }
+  *colon = '\0';
+  listenfd = setup_listener(hp, colon + 1);
+
+  printf("Server listening on %s:%s\n", hp, colon + 1);
+  fflush(stdout);
+  free(hp);
 }
